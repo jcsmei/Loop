555669d6c4b82efb7e77f0ed35e48ceaea35b747
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:13:03 -0500

    Add project files.

---
+++ Loop/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Loop
+{
+    partial class frmLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.btnForLoop = new System.Windows.Forms.Button();
+            this.btnTables = new System.Windows.Forms.Button();
+            this.btnPassword = new System.Windows.Forms.Button();
+            this.btnPractice = new System.Windows.Forms.Button();
+            this.btnThreeTimes = new System.Windows.Forms.Button();
+            this.btnCLear = new System.Windows.Forms.Button();
+            this.btnMagic = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.BackColor = System.Drawing.Color.White;
+            this.txtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDisplay.Size = new System.Drawing.Size(600, 164);
+            this.txtDisplay.TabIndex = 0;
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWhile.Location = new System.Drawing.Point(12, 192);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(100, 35);
+            this.btnWhile.TabIndex = 1;
+            this.btnWhile.Text = "While Loop";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDo
+            // 
+            this.btnDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDo.Location = new System.Drawing.Point(12, 247);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(100, 35);
+            this.btnDo.TabIndex = 2;
+            this.btnDo.Text = "Do Loop";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // btnForLoop
+            // 
+            this.btnForLoop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForLoop.Location = new System.Drawing.Point(138, 192);
+            this.btnForLoop.Name = "btnForLoop";
+            this.btnForLoop.Size = new System.Drawing.Size(100, 35);
+            this.btnForLoop.TabIndex = 3;
+            this.btnForLoop.Text = "For Loop";
+            this.btnForLoop.UseVisualStyleBackColor = true;
+            this.btnForLoop.Click += new System.EventHandler(this.btnForLoop_Click);
+            // 
+            // btnTables
+            // 
+            this.btnTables.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTables.Location = new System.Drawing.Point(263, 192);
+            this.btnTables.Name = "btnTables";
+            this.btnTables.Size = new System.Drawing.Size(100, 35);
+            this.btnTables.TabIndex = 4;
+            this.btnTables.Text = "Tables";
+            this.btnTables.UseVisualStyleBackColor = true;
+            this.btnTables.Click += new System.EventHandler(this.btnTables_Click);
+            // 
+            // btnPassword
+            // 
+            this.btnPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPassword.Location = new System.Drawing.Point(138, 247);
+            this.btnPassword.Name = "btnPassword";
+            this.btnPassword.Size = new System.Drawing.Size(100, 35);
+            this.btnPassword.TabIndex = 5;
+            this.btnPassword.Text = "Password";
+            this.btnPassword.UseVisualStyleBackColor = true;
+            this.btnPassword.Click += new System.EventHandler(this.btnPassword_Click);
+            // 
+            // btnPractice
+            // 
+            this.btnPractice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPractice.Location = new System.Drawing.Point(383, 192);
+            this.btnPractice.Name = "btnPractice";
+            this.btnPractice.Size = new System.Drawing.Size(100, 35);
+            this.btnPractice.TabIndex = 6;
+            this.btnPractice.Text = "Practice";
+            this.btnPractice.UseVisualStyleBackColor = true;
+            this.btnPractice.Click += new System.EventHandler(this.btnPractice_Click);
+            // 
+            // btnThreeTimes
+            // 
+            this.btnThreeTimes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThreeTimes.Location = new System.Drawing.Point(263, 247);
+            this.btnThreeTimes.Name = "btnThreeTimes";
+            this.btnThreeTimes.Size = new System.Drawing.Size(100, 35);
+            this.btnThreeTimes.TabIndex = 7;
+            this.btnThreeTimes.Text = "3 Times";
+            this.btnThreeTimes.UseVisualStyleBackColor = true;
+            this.btnThreeTimes.Click += new System.EventHandler(this.btnThreeTimes_Click);
+            // 
+            // btnCLear
+            // 
+            this.btnCLear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCLear.Location = new System.Drawing.Point(535, 192);
+            this.btnCLear.Name = "btnCLear";
+            this.btnCLear.Size = new System.Drawing.Size(77, 35);
+            this.btnCLear.TabIndex = 8;
+            this.btnCLear.Text = "&CLEAR";
+            this.btnCLear.UseVisualStyleBackColor = true;
+            this.btnCLear.Click += new System.EventHandler(this.btnCLear_Click);
+            // 
+            // btnMagic
+            // 
+            this.btnMagic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMagic.Location = new System.Drawing.Point(383, 247);
+            this.btnMagic.Name = "btnMagic";
+            this.btnMagic.Size = new System.Drawing.Size(100, 35);
+            this.btnMagic.TabIndex = 9;
+            this.btnMagic.Text = "Magic Number";
+            this.btnMagic.UseVisualStyleBackColor = true;
+            this.btnMagic.Click += new System.EventHandler(this.btnMagic_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(535, 247);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(77, 35);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "E&XIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 292);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnMagic);
+            this.Controls.Add(this.btnCLear);
+            this.Controls.Add(this.btnThreeTimes);
+            this.Controls.Add(this.btnPractice);
+            this.Controls.Add(this.btnPassword);
+            this.Controls.Add(this.btnTables);
+            this.Controls.Add(this.btnForLoop);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.txtDisplay);
+            this.Name = "frmLoop";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Button btnForLoop;
+        private System.Windows.Forms.Button btnTables;
+        private System.Windows.Forms.Button btnPassword;
+        private System.Windows.Forms.Button btnPractice;
+        private System.Windows.Forms.Button btnThreeTimes;
+        private System.Windows.Forms.Button btnCLear;
+        private System.Windows.Forms.Button btnMagic;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Loop/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Loop
+{
+    public partial class frmLoop : Form
+    {
+        public frmLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnForLoop_Click(object sender, EventArgs e)
+        {
+            for (int c = 1; c <= 81; c += 1)
+            {
+                txtDisplay.Text += c.ToString() + Environment.NewLine;
+            }
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            int c = 0;
+            do
+            {
+                txtDisplay.Text += c.ToString() + Environment.NewLine;
+                c += 100;
+            } while (c <= 1000);
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            int c = 1;
+            while (c <= 5)
+            {
+                txtDisplay.Text += c.ToString() + Environment.NewLine;
+                c += 1;
+            }
+        }
+
+        private void btnPassword_Click(object sender, EventArgs e)
+        {
+            string strPass = "cit100";
+            string strUser = Interaction.InputBox("Please enter your password.");
+            int c = 1;
+            while ((strUser != strPass))
+            {
+                if (c < 3)
+                {
+                    strUser = Interaction.InputBox("Wrong password. Try again.");
+                    c += 1;
+                }
+                else
+                {
+                    MessageBox.Show("You've tried 3 times already. You are locked Out.");
+                    return;
+                }
+            }
+            MessageBox.Show("You are now logged in.");
+        }
+
+        private void btnTables_Click(object sender, EventArgs e)
+        {
+            //for (int c = 1; c <= 100; c++)
+            //{
+            //    txtDisplay.Text += "100 +" + c.ToString() + " = " +
+            //        (100 + c).ToString() + Environment.NewLine;
+            //}
+            //for (int x = 0; x <= 32; x++)
+            //{
+            //    txtDisplay.Text += "2 ^ " + x.ToString() + " = " +
+            //        (Math.Pow(2, x)).ToString() + Environment.NewLine;
+            //}
+            //for (int c = 1; c <= 99; c++)
+            //{
+            //    txtDisplay.Text += "99 *" + c.ToString() + " = " +
+            //        (99 * c).ToString() + Environment.NewLine;
+            //}
+            //for (int c = 1; c <= 99; c += 1)
+            //{
+            //    txtDisplay.Text += "99 * " + c.ToString() + " = " +
+            //        (99 * c).ToString() + Environment.NewLine;
+
+            //}
+            for (int c = 1; c <= 9 ; c += 1)
+            {
+                txtDisplay.Text += c.ToString() + " ^ 2 = " +
+                    (c * c).ToString() + Environment.NewLine;
+            }
+        }
+
+        private void btnThreeTimes_Click(object sender, EventArgs e)
+        {
+            int n = 1;
+            while (n <= 100000)
+            {
+                txtDisplay.Text += n.ToString() + Environment.NewLine;
+                n = n * 3;
+            }
+            //double x = 1000;
+            //int c = 1;
+            //while (c <= 20)
+            //{
+            //    txtDisplay.Text += (x).ToString() + Environment.NewLine;
+            //    x /= 2;
+            //    c++;
+            //}
+        }
+
+        private void btnCLear_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = " ";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPractice_Click(object sender, EventArgs e)
+        {
+            //    for (int c = 1; c <= 101; c++)
+            //    {
+            //        txtDisplay.Text += c.ToString() + " * " +
+            //            (c - 1).ToString() + " = " +
+            //            (c * (c - 1)).ToString() + Environment.NewLine;
+            //    }
+            //    for (int c = 10; c <= 100; c++)
+            //    {
+            //        txtDisplay.Text += c.ToString() + " * 2 = " +
+            //            (c * 2).ToString() + Environment.NewLine;
+            //    }
+            //for (int c = 10; c >= 0; c--)
+            //{
+            //    txtDisplay.Text += c.ToString() + Environment.NewLine;
+            //    txtDisplay.Text += "you are number " + c.ToString() +
+            //        " good programmer" + Environment.NewLine;
+            //}
+            for (decimal c = 11; c <= 555; c = c + 3)
+            {
+                //txtDisplay.Text += c.ToString("n2") + Environment.NewLine;
+                txtDisplay.Text += "You are number " + c.ToString() +
+                    " good programmer" + Environment.NewLine;
+            }
+        }
+
+        private void btnMagic_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int MagicNumber = r.Next(1, 101);
+            int totalTrial = 1;
+            int intGuess = 0;
+            string strGuess = null;
+            txtDisplay.Text = "Game Rule: A magic number between 1 and 100 have been genrated." +
+                Constants.vbNewLine + "Try to guess it in as few times as possible.";
+
+            do
+            {
+                do
+                {
+                    strGuess = Interaction.InputBox("What is the magic number?");
+                } while (!(int.TryParse(strGuess, out intGuess)));
+
+                if (intGuess > MagicNumber)
+                {
+                    txtDisplay.Text = totalTrial.ToString() + " trial: " + intGuess.ToString() +
+                        " is larger than the magic number." + Environment.NewLine + txtDisplay.Text;
+                }
+                else if (intGuess < MagicNumber)
+                {
+                    txtDisplay.Text = totalTrial.ToString() + " trial: " +
+                        intGuess.ToString() + " is smaller than the magic number." +
+                        Environment.NewLine + txtDisplay.Text;
+                }
+                totalTrial += 1;
+            } while (intGuess != MagicNumber);
+            txtDisplay.Text = intGuess.ToString() + " is the magic number. You have tried " +
+                (totalTrial - 1).ToString() + " times" + Environment.NewLine + Environment.NewLine +
+                txtDisplay.Text;   
+            }
+            }
+    }
+
+
+
+            
+            
+    
+
+        
+    
+
+
